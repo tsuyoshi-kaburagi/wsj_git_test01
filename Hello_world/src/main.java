805f97adfead2707bda425dabb98c2b9d877ec6a
tsuyoshi-kaburagi <37562868+tsuyoshi-kaburagi@users.noreply.github.com>
2018-04-02 18:27:58 +0900

    This is a direct push test from local repository (test03)

--- Hello_world/src/main.java
+++ Hello_world/src/main.java
@@ -4,9 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("This is 3rd attempt");
 		System.out.println("Hello world");
-		System.out.println("This is second attempt for git test");
-		System.out.println("Conflict pattern");
+//		System.out.println("This is second attempt for git test");
 
 	}
 
