7d7e1ae92310a6943fc5802873b1c30047da0f51
tsuyoshi-kaburagi <37562868+tsuyoshi-kaburagi@users.noreply.github.com>
2018-04-02 20:12:39 +0900

    This is the 4th commit from cloned project.

--- Hello_world/src/main.java
+++ Hello_world/src/main.java
@@ -4,7 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is 3rd attempt");
+		System.out.println("This is 4th attempt");
+		System.out.println("This is clone project");
 		System.out.println("Hello world");
 //		System.out.println("This is second attempt for git test");
 
